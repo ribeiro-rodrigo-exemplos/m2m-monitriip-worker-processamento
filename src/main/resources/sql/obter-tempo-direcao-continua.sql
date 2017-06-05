f80a844cb45a3a5c3ea578632834f10f65043b26
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2017-06-05 17:04:47 -0300

    correcao busca direcao continua e jornada de trabalho

--- src/main/resources/sql/obter-tempo-direcao-continua.sql
+++ src/main/resources/sql/obter-tempo-direcao-continua.sql
@@ -1,2 +1,2 @@
 select id_cliente,nu_max_minutos as tempo from direcao_continua
-  where id_cliente=:#${property.originalPayload[idCliente]}
+  where id_cliente=:#${property.originalPayload[idCliente]} order by id_direcao_continua desc limit 1;
--- src/main/resources/sql/obter-tempo-jornada.sql
+++ src/main/resources/sql/obter-tempo-jornada.sql
@@ -1,2 +1,2 @@
 select id_cliente,nu_max_minutos as tempo from jornada_trabalho as jornada
-  where id_cliente=:#${property.payload[idCliente]}
+  where id_cliente=:#${property.payload[idCliente]} order by id_jornada_trabalho desc limit 1;
