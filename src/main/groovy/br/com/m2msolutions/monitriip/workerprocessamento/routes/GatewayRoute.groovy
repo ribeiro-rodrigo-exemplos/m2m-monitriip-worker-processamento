ad9aac4ec88e298a78e8716c610cd86b42f99c8c
Rodrigo Ribeiro <rodrigo.rs1989@gmail.com>
2017-06-15 20:26:08 -0300

    colocando parametro concurrentConsumers do rabbit no arquivo de configurações

--- src/main/groovy/br/com/m2msolutions/monitriip/workerprocessamento/routes/GatewayRoute.groovy
+++ src/main/groovy/br/com/m2msolutions/monitriip/workerprocessamento/routes/GatewayRoute.groovy
@@ -20,7 +20,8 @@ class GatewayRoute extends RouteBuilder {
 
         from("rabbitmq://${rcfg.url}/${rcfg.exchange}?queue=${rcfg.queue}&deadLetterExchange=${rcfg['exchange-dlq']}" +
                 "&deadLetterQueue=${rcfg.deadLetterQueue}&deadLetterRoutingKey=dead.letters&autoAck=false&" +
-                "autoDelete=false&concurrentConsumers=10&username=${rcfg.username}&password=${rcfg.password}").
+                "autoDelete=false&concurrentConsumers=${rcfg.concurrentConsumers}&username=${rcfg.username}" +
+                "&password=${rcfg.password}").
             unmarshal().string().
             convertBodyTo(Map).
             choice().
